400473a475f99b98fc52a3e2c87b1f0642bf6e88
carlos47fnaf <kaduardomello@gmail.com>
2023-03-07 17:00:53 -0300

    appcontador

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import Counter from './src/components/Counter';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Counter />
+     <Counter />
+     <Counter />
+     <Counter />
     </View>
   );
 }
